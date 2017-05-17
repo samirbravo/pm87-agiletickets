66c390f83d1f6088e0b4f92c795c342d4addaa6b
Samir Bravo <sambravo@gmail.com>
2017-05-17 20:24:00 -0300

    refatoração do metodo calcula da classe CalculadoraDePrecos

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -8,41 +8,25 @@ import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
-
-		if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA)
-				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			preco = calcularPrecoGeral(sessao, 0.05, 0.10);
-		} else if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)
-				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			preco = calcularPrecoComTempo(sessao, 0.50, 0.20, 60, 0.10);
-		} else {
-			preco = sessao.getPreco();
+		
+		BigDecimal precoSessao = sessao.getPreco();
+		Integer totalIngresso = sessao.getTotalIngressos();
+		Integer ingressosReservados = sessao.getIngressosReservados();
+		TipoDeEspetaculo tipoEspetaculo = sessao.getEspetaculo().getTipo();
+		
+		if ((totalIngresso - ingressosReservados) / totalIngresso.doubleValue() <= tipoEspetaculo.getPercentualMinimo()) {
+			precoSessao = precoSessao.add(precoSessao.multiply(BigDecimal.valueOf(tipoEspetaculo.getFatorAumento())));
+		} 
+		
+		if (tipoEspetaculo.equals(TipoDeEspetaculo.BALLET) || tipoEspetaculo.equals(TipoDeEspetaculo.ORQUESTRA)) {
+			if (sessao.getDuracaoEmMinutos() > 60) {
+				precoSessao = precoSessao.add(sessao.getPreco().multiply(BigDecimal.valueOf(tipoEspetaculo.getPercentualDuracao())));
+			}
 		}
 
-		return preco.multiply(BigDecimal.valueOf(quantidade));
+		return precoSessao.multiply(BigDecimal.valueOf(quantidade));
 	}
 
-	private static BigDecimal calcularPrecoGeral(Sessao sessao, double percentualMinimo, double fatorAumento) {
-
-		if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
-				/ sessao.getTotalIngressos().doubleValue() <= percentualMinimo) {
-			return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(fatorAumento)));
-		} else {
-			return sessao.getPreco();
-		}
-	}
 	
-	private static BigDecimal calcularPrecoComTempo(Sessao sessao, double percentualMinimo, double fatorAumento, int duracao, double percentualDuracao){
-		BigDecimal preco;
-		
-		preco = calcularPrecoGeral(sessao, percentualMinimo, fatorAumento);
-		
-		if (sessao.getDuracaoEmMinutos() > duracao) {
-			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(percentualDuracao)));
-		}
-		
-		return preco;
-	}
 
 }
--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -2,6 +2,36 @@ package br.com.caelum.agiletickets.models;
 
 public enum TipoDeEspetaculo {
 	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
+	CINEMA(0.05, 0.10), SHOW(0.05, 0.10), TEATRO(), BALLET(0.50, 0.20, 0.10), ORQUESTRA(0.50, 0.20, 0.10);
+	
+	private double percentualMinimo;
+	private double fatorAumento;
+	private double percentualDuracao;
+	
+	TipoDeEspetaculo(){
+		
+	}
+	
+	TipoDeEspetaculo(double percentualMinimo, double fatorAumento){
+		this.percentualMinimo = percentualMinimo;
+		this.fatorAumento = fatorAumento;
+	}
+	
+	TipoDeEspetaculo(double percentualMinimo, double fatorAumento, double percentualDuracao){
+		this.percentualMinimo = percentualMinimo;
+		this.fatorAumento = fatorAumento;
+		this.percentualDuracao = percentualDuracao;
+	}
+	
+	public double getPercentualMinimo() {
+		return percentualMinimo;
+	}
+	public double getFatorAumento() {
+		return fatorAumento;
+	}
+	public double getPercentualDuracao() {
+		return percentualDuracao;
+	}
+	
 	
 }
