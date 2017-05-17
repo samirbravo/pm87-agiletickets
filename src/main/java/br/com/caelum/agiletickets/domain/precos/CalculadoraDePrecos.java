6ceaf0386634b38f7e265cbd001981906a534e72
Samir Bravo <sambravo@gmail.com>
2017-05-16 23:01:28 -0300

    criação do metodo calcularPrecoComTempo e implementação para Ballet e Orchestra

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -6,56 +6,43 @@ import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
-	
-	
-	
+
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
 
 		if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA)
 				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			// quando estiver acabando os ingressos...
 			preco = calcularPrecoGeral(sessao, 0.05, 0.10);
 		} else if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)
 				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
-					/ sessao.getTotalIngressos().doubleValue() <= 0.50) {
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-
-			if (sessao.getDuracaoEmMinutos() > 60) {
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-//		} else if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-//			if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
-//					/ sessao.getTotalIngressos().doubleValue() <= 0.50) {
-//				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-//			} else {
-//				preco = sessao.getPreco();
-//			}
-//
-//			if (sessao.getDuracaoEmMinutos() > 60) {
-//				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-//			}
+			preco = calcularPrecoComTempo(sessao, 0.50, 0.20, 60, 0.10);
 		} else {
-			// nao aplica aumento para teatro (quem vai é pobretão)
 			preco = sessao.getPreco();
 		}
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
-	
-	private static BigDecimal calcularPrecoGeral(Sessao sessao, double percentualMinimo, double fatorAumento){
-		
+
+	private static BigDecimal calcularPrecoGeral(Sessao sessao, double percentualMinimo, double fatorAumento) {
+
 		if ((sessao.getTotalIngressos() - sessao.getIngressosReservados())
 				/ sessao.getTotalIngressos().doubleValue() <= percentualMinimo) {
 			return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(fatorAumento)));
 		} else {
 			return sessao.getPreco();
 		}
-
+	}
+	
+	private static BigDecimal calcularPrecoComTempo(Sessao sessao, double percentualMinimo, double fatorAumento, int duracao, double percentualDuracao){
+		BigDecimal preco;
+		
+		preco = calcularPrecoGeral(sessao, percentualMinimo, fatorAumento);
+		
+		if (sessao.getDuracaoEmMinutos() > duracao) {
+			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(percentualDuracao)));
+		}
+		
+		return preco;
 	}
 
 }
